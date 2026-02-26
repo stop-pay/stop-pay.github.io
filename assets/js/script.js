b9442aa2e93bb4bec4bb51df06123f337a30d8ab
pandadreamgs <pandadreamgs@gmail.com>
2026-02-26 12:35:28 +0000

    Deploying to gh-pages from @ pandadreamgs/stop_pay@cc10649e46c454f0f5ec8830ae23a547128eea02 🚀

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ let siteData = null;
 let totalSavedUsd = 0;
 
 const isGithub = window.location.hostname.includes('github.io');
-const BASE_URL = isGithub ? '/stop_pay' : ''; 
+const isSubfolder = window.location.hostname.includes('github.io') && window.location.pathname.startsWith('/stop_pay');
+const BASE_URL = isSubfolder ? '/stop_pay' : ''; 
 const BRIDGE_URL = "https://script.google.com/macros/s/AKfycbywfH00K-KVqfhkPQwWy4P2Knaa0hS1KP1TD6zDfn2K9Bd31Td1pPRxGRj5t1Xt7j1voQ/exec"; 
 
 // --- 1. ІНІЦІАЛІЗАЦІЯ ПРИ ЗАВАНТАЖЕННІ ---
